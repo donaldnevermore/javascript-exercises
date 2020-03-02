f5e2912a84343217a2ec892d869445d81499df05
Dongcheng Wang <1219782000@qq.com>
2020-03-02 18:55:16 +0800

    Chore

--- interviews/palindrome.test.js
+++ interviews/palindrome.test.js
@@ -1,6 +1,6 @@
 const isPalindrome = require('./palindrome')
 
-isType('回文字符串', () => {
-  expect(isPalindrome('aba')).toBe(true)
-  expect(isPalindrome('abc')).toBe(false)
+test('回文字符串', () => {
+    expect(isPalindrome('aba')).toBe(true)
+    expect(isPalindrome('abc')).toBe(false)
 })
