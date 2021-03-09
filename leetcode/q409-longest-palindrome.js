4ea14b5acd783b9e30057a86d5f61fe813384284
Donald33 Wang <dongcheng6@qq.com>
2021-03-10 07:37:35 +0800

    Rename question 002 & add question 409

---
+++ leetcode/q409-longest-palindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const longestPalindrome = function (s) {
+    const counter = new Set()
+    let sum = 0
+
+    for (const ch of s) {
+        if (counter.has(ch)) {
+            counter.delete(ch)
+            sum += 2
+        } else {
+            counter.add(ch)
+        }
+    }
+
+    return sum + (counter.size > 0 ? 1 : 0)
+}
+
+const r = longestPalindrome("abccccdd")
+console.log(r)
