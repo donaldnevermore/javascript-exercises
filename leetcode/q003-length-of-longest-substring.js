6b21e37942514471cf0ef7072490b4eb28afcc6c
Donald33 Wang <dongcheng6@qq.com>
2021-03-10 22:47:30 +0800

    Add question 003

---
+++ leetcode/q003-length-of-longest-substring.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = function (s) {
+    let curLength = 0
+    let maxLength = 0
+
+    const position = new Map()
+
+    for (let i = 0; i < s.length; i++) {
+        let prevIndex
+        if (position.has(s[i])) {
+            prevIndex = position.get(s[i])
+        } else {
+            prevIndex = -1
+        }
+
+        if (prevIndex < 0 || i - prevIndex > curLength) {
+            curLength++
+        } else {
+            if (curLength > maxLength) {
+                maxLength = curLength
+            }
+
+            curLength = i - prevIndex
+        }
+
+        position.set(s[i], i)
+    }
+
+    if (curLength > maxLength) {
+        maxLength = curLength
+    }
+
+    return maxLength
+}
+
+const r = lengthOfLongestSubstring("arabcacfr")
+console.log(r)
