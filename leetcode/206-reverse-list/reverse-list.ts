58977508a1daa00aefaa1f8028046baa3ac6fa94
Donald D. Nevermore <donald33wang@gmail.com>
2021-12-02 20:17:25 +0800

    Add 206

---
+++ leetcode/206-reverse-list/reverse-list.ts
@@ -0,0 +1,21 @@
+import { ListNode } from "../shared"
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let prev: ListNode | null = null
+    let reverseHead: ListNode | null = null
+    let node = head
+
+    while (node !== null) {
+        const next = node.next
+
+        if (next === null) {
+            reverseHead = node
+        }
+
+        node.next = prev
+        prev = node
+        node = next
+    }
+
+    return reverseHead
+}
