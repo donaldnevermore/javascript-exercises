bb490c423bde7ec8f8761fdbccd1b4bcfff558da
Donald33 D. Nevermore <donald33wang@gmail.com>
2022-05-28 23:51:32 +0800

    Add 0202

---
+++ cracking-coding-interview/0102-check-permutation.ts
@@ -0,0 +1,34 @@
+// A simple method: sort 2 strings and compare them.
+
+function checkPermutation(s1: string, s2: string): boolean {
+    if (s1.length !== s2.length) {
+        return false
+    }
+
+    const charCount: number[] = []
+
+    for (let i = 0; i < s1.length; i++) {
+        const ch = s1.charCodeAt(i)
+        if (charCount[ch]) {
+            charCount[ch]++
+        }
+        else {
+            charCount[ch] = 1
+        }
+    }
+
+    for (let j = 0; j < s2.length; j++) {
+        const ch = s2.charCodeAt(j)
+        if (charCount[ch] && charCount[ch] > 0) {
+            charCount[ch]--
+        }
+        else {
+            return false
+        }
+    }
+
+    return true
+}
+
+console.log(checkPermutation("abc", "bca"))
+console.log(checkPermutation("abc", "bad"))
