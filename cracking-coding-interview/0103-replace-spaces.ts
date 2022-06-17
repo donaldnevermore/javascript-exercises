099adb69caf776c39619d00047316d1b9aceca33
Donald33 D. Nevermore <donald33wang@gmail.com>
2022-06-17 21:44:18 +0800

    Add 0103

---
+++ cracking-coding-interview/0103-replace-spaces.ts
@@ -0,0 +1,29 @@
+function replaceSpaces(s: string, length: number): string {
+    let spaceCount = 0
+    for (let i = 0; i < length; i++) {
+        if (s[i] === " ") {
+            spaceCount++
+        }
+    }
+
+    let len = length + spaceCount * 2
+
+    const arr: string[] = []
+    for (let j = length - 1; j >= 0; j--) {
+        if (s[j] === " ") {
+            arr[len - 1] = "0"
+            arr[len - 2] = "2"
+            arr[len - 3] = "%"
+            len -= 3
+        }
+        else {
+            arr[len - 1] = s[j]
+            len--
+        }
+
+    }
+
+    return arr.join("")
+}
+
+console.log(replaceSpaces("a b", 3))
