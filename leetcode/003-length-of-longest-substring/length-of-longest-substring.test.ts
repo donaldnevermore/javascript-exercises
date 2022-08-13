a4c6c11419117ab68657162ecfbdcbfae1de4373
Donald33 D. Nevermore <donald33wang@gmail.com>
2022-08-13 19:19:58 +0800

    Add 167 & chores

--- leetcode/003-length-of-longest-substring/length-of-longest-substring.test.ts
+++
@@ -1,6 +0,0 @@
-import { lengthOfLongestSubstring } from "./length-of-longest-substring"
-
-test("length of longest substring", () => {
-    const n = lengthOfLongestSubstring("arabcacfr")
-    expect(n).toBe(4)
-})
---
+++ leetcode/167-two-sum/two-sum.ts
@@ -0,0 +1,22 @@
+function twoSum(numbers: number[], target: number): number[] {
+    let left = 0
+    let right = numbers.length - 1
+
+    while (left < right) {
+        const sum = numbers[left] + numbers[right]
+        if (sum === target) {
+            return [left + 1, right + 1]
+        }
+        else if (sum < target) {
+            left++
+        }
+        else {
+            right--
+        }
+    }
+
+    return []
+}
+
+const res = twoSum([2, 7, 11, 15], 9)
+console.log(res)
