032e52a528b1890801862a29df7c96e77254e1cd
Donald33 D. Nevermore <donald33wang@gmail.com>
2022-09-17 22:35:38 +0800

    Add 524

---
+++ leetcode/524-find-longest-word/find-longest-word.ts
@@ -0,0 +1,29 @@
+function findLongestWord(s: string, dictionary: string[]): string {
+    let longest = ""
+
+    for (const word of dictionary) {
+        if (longest.length > word.length || (longest.length === word.length && longest < word)) {
+            continue
+        }
+
+        if (isSubstring(s, word)) {
+            longest = word
+        }
+    }
+
+    return longest
+}
+
+function isSubstring(s: string, word: string): boolean {
+    let i = 0
+    let j = 0
+
+    while (i < s.length && j < word.length) {
+        if (s[i] === word[j]) {
+            j++
+        }
+        i++
+    }
+
+    return j === word.length
+}
