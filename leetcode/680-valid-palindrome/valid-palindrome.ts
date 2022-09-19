6595828d8473a5c4c5eb54823d431f2cbadf483b
Donald33 D. Nevermore <donald33wang@gmail.com>
2022-09-19 21:34:34 +0800

    Add 680

---
+++ leetcode/680-valid-palindrome/valid-palindrome.ts
@@ -0,0 +1,26 @@
+function validPalindrome(s: string): boolean {
+    let left = 0
+    let right = s.length - 1
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1)
+        }
+        left++
+        right--
+    }
+
+    return true
+}
+
+function isPalindrome(s: string, left: number, right: number): boolean {
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false
+        }
+        left++
+        right--
+    }
+
+    return true
+}
