783a01c61e20d95404dc7f566d83b00b75a6ef02
Donald33 D. Nevermore <donald33wang@gmail.com>
2022-09-24 21:30:11 +0800

    Add 633

---
+++ leetcode/633-judge-square-sum/judge-square-sum.ts
@@ -0,0 +1,23 @@
+function judgeSquareSum(c: number): boolean {
+    if (c < 0) {
+        return false
+    }
+
+    let left = 0
+    let right = Math.floor(Math.sqrt(c))
+
+    while (left <= right) {
+        const sum = left * left + right * right
+        if (sum < c) {
+            left++
+        }
+        else if (sum > c) {
+            right--
+        }
+        else {
+            return true
+        }
+    }
+
+    return false
+}
