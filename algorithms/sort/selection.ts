d48ff253f33c09394f25ec3f544a93057a49525a
Donald33 Wang <donald33wang@gmail.com>
2022-12-09 15:23:53 +0800

    Update selection sort

---
+++ algorithms/sort/selection.ts
@@ -0,0 +1,21 @@
+/**
+ * 选择排序
+ * 不稳定
+ *
+ * 平均情况：T(n) = O(n^2)
+ * 最佳情况：T(n) = O(n^2)
+ * 最差情况：T(n) = O(n^2)
+ */
+function selectionSort(arr: number[]): void {
+    const n = arr.length
+    for (let i = 0; i < n; i++) {
+        let min = i
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                // Find out the smallest one.
+                min = j
+            }
+        }
+        [arr[i], arr[min]] = [arr[min], arr[i]]
+    }
+}
